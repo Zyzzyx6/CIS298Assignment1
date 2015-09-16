ce9cd592f7c00f65d29e06e22ac78d7e5467e1ce
Marty Russon <martyrusson@gmail.com>
2015-09-16 14:18:15 -0400

    Update java and layout files

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,13 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
+
+    private Button mPlusButton;
+    private Button mMinusButton;
+    private TextView textout;
+
+    int n1 = 0;
+    int ans = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+
+        mPlusButton = (Button) findViewById(R.id.plus_button);
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+                //Do something here
+                int ans = n1++;
+
+                final TextView txtValue = (TextView) findViewById(R.id.people_counter);
+                txtValue.setText(Integer.toString(ans));
+
+                //System.out.println(ans);
+
+               // textout.setText(ans);
+
+              //  setText(ans);
+               // n1 = ans;
+            }
+
+        });
+
     }
 
     @Override
@@ -20,7 +52,11 @@ public class CounterActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
+/*int a = 2;
+    int b = 3;
+    System.out.println(a+b);
+    */
+        @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
---
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/OnClickListener.java
@@ -0,0 +1,7 @@
+package edu.kvcc.cis298.cis298assignment1;
+
+/**
+ * Created by Marty on 9/15/2015.
+ */
+public class OnClickListener {
+}
